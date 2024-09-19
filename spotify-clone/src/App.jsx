732cecfafb45d580bcd2e4266838a3f61a52722c
Tharindu dasantha <thari2001dasantha@gmail.com>
2024-09-19 18:22:22 +0530

    Full Stack Spotify Clone App

---
+++ spotify-clone/src/App.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+
+const App = () => {
+  const {audioRef, track, songsData} = useContext(PlayerContext)
+
+  return (
+    <div className="h-screen bg-black">
+      {
+        songsData.length !== 0
+        ? <>
+            <div className="h-[90%] flex">
+              <Sidebar/>
+              <Display />
+            </div>
+              <Player/>
+            <audio ref={audioRef} src={track ? track.file : ""} preload="auto"></audio>
+          </> : null
+      }
+    </div>
+  )
+}
+
+export default App
